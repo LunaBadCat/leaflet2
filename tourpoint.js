ef99af80eb40c2cff73bfba2988f07b9c68f0cde
LunaBadCat <124545699+LunaBadCat@users.noreply.github.com>
2023-04-25 23:38:44 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Bangkok",
+          "ประเภท": "Culture",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.49228368776062,
+            13.754105580425929
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Ratchaburi",
+          "ประเภท": "Natural",
+          "ระดับ": 8
+        },
+        "geometry": {
+          "coordinates": [
+            99.82123665197975,
+            13.571396848727133
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Hua Hin",
+          "ประเภท": "Natural",
+          "ระดับ": 9
+        },
+        "geometry": {
+          "coordinates": [
+            99.94845844540117,
+            12.569783875352726
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Pattaya",
+          "ประเภท": "Natural/Culture",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.890851541801,
+            12.943747420056283
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Chiang Mai",
+          "ประเภท": "Natural/Adventure",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.93708149332838,
+            18.834636525806403
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Phuket",
+          "ประเภท": "Natural/Adventure",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.29526978477651,
+            7.991268600463201
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Samui",
+          "ประเภท": "Natural/Island",
+          "ระดับ": 9
+        },
+        "geometry": {
+          "coordinates": [
+            99.99146562226656,
+            9.57965321845748
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Khao Yai",
+          "ประเภท": "National Park",
+          "ระดับ": 8
+        },
+        "geometry": {
+          "coordinates": [
+            101.50127300147977,
+            14.333201866155164
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Srinagarindra Dam",
+          "ประเภท": "National Park",
+          "ระดับ": 7
+        },
+        "geometry": {
+          "coordinates": [
+            99.04021266002547,
+            14.728327226542788
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อแหล่งท่องเที่ยว": "Chiang Kan",
+          "ประเภท": "Culture",
+          "ระดับ": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.65236433046903,
+            17.89148212534809
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
